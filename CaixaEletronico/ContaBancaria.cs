898ea44a3ecb9806a7065f03a7193aac10401a25
gabri <gabri@DESKTOP-KLVDFMS>
2021-11-20 14:39:04 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEletronico/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CaixaEletronico
+{
+    internal class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; } //privado pois só vai poder ser alterado pela função Depositar ou Sacar.
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+            
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito (double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;  //os 5.0 adicionados representam a taxa solicitada no exercicio.
+        }
+
+        public override string ToString()
+        {
+            return "Conta"
+                + Numero
+                + ", titular "
+                + Titular
+                + ", Saldo: "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+                ;
+
+        }
+    }
+}
---
+++ CaixaEletronico/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+namespace CaixaEletronico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            ContaBancaria saldo;
+           
+
+            Console.WriteLine("========= Banco Diamantino =========");
+            Console.WriteLine("-------- Seja bem vindo(a) ---------");
+            Console.Write("Digite o número da conta:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("====================================");
+
+            Console.Write("Digite o nome do Titular:");
+            string titular = Console.ReadLine();
+
+            Console.WriteLine("====================================");
+
+            Console.WriteLine("Deseja fazer um deposito inicial?");
+            Console.WriteLine("S- Sim");
+            Console.WriteLine("N - NÃO");
+            
+            char resposta = char.Parse(Console.ReadLine());
+
+            
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.Write("Digite o valor que deseja depositar:");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+                Console.WriteLine("Dados da conta");
+                Console.Write($"Titular: {titular}.");
+                Console.Write($"Nº da Conta: {conta}");
+                Console.Write($"Saldo: R${depositoInicial}");
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine("Dados da conta");
+            Console.Write($"Titular: {titular}.");
+            Console.Write($"Nº da Conta: {conta}");
+
+            Console.WriteLine("====================================");
+            Console.Write("Digite um Valor para deposito:");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.Write("Dados Atualizados:");
+            Console.WriteLine(conta);
+
+            Console.WriteLine("====================================");
+            Console.Write("Digite um Valor para saque:");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.Write("Dados Atualizados:");
+            Console.WriteLine(conta);
+            Console.Write("Lem-se a taxa de saque é de R$5.00.");
+
+        }
+    }
+}
